5ce81300863ab0987c654003cf2b4373523e6cc8
Alan Cunningham <ajc.alan@gmail.com>
2025-08-28 22:55:24 +0100

    Add no events

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ from PIL import Image, ImageDraw, ImageFont
 
 def main():
     display = InkyPHAT("black")
+    width = 212
+    height = 104
 
     events = google_calendar.get_todays_events()
 
     image = Image.new(mode="RGB", size=display.resolution, color=(255, 255, 255))
     draw = ImageDraw.Draw(image)
     chikarego_font = ImageFont.truetype("ChiKareGo.ttf", 16)
-    leco_font = ImageFont.truetype("leco.ttf", 16)
 
     now = datetime.now()
     day_of_week = datetime.strftime(now, "%A")
@@ -29,29 +30,40 @@ def main():
     date_title = f"{day_of_week} {day_of_month}{suffix} {month}"
     draw.text((0, 0), date_title, display.BLACK, font=chikarego_font)
 
-    starting_event_y_coords = 30
-    max_summary_character_length = 23
-    for event in events:
-        event_time, event_summary = event
-        # Draw the event time
+    if not events:
+        # No events
         draw.text(
-            (0, starting_event_y_coords),
-            f"{event_time}:",
+            (width / 2, height / 2),
+            "No events today",
             display.BLACK,
+            anchor="mm",
             font=chikarego_font,
         )
+    else:
+        starting_event_y_coords = 30
+        max_summary_character_length = 23
 
-        # Draw the event summary
-        if len(event_summary) > max_summary_character_length:
-            event_summary = event_summary[:max_summary_character_length] + "..."
-        draw.text(
-            (50, starting_event_y_coords),
-            event_summary,
-            display.BLACK,
-            font=chikarego_font,
-        )
+        for event in events:
+            event_time, event_summary = event
+            # Draw the event time
+            draw.text(
+                (0, starting_event_y_coords),
+                f"{event_time}:",
+                display.BLACK,
+                font=chikarego_font,
+            )
 
-        starting_event_y_coords += 20
+            # Draw the event summary
+            if len(event_summary) > max_summary_character_length:
+                event_summary = event_summary[:max_summary_character_length] + "..."
+            draw.text(
+                (50, starting_event_y_coords),
+                event_summary,
+                display.BLACK,
+                font=chikarego_font,
+            )
+
+            starting_event_y_coords += 20
 
     display.set_image(image)
     display.show()
